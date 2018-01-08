013e690b1a52f276ddc260a958a06c4827776942
junho <kanndu9@gmail.com>
2018-01-08 21:07:22 +0900

    SprictTestの初めてのコミット
    SprictTestの初めてのコミット

---
+++ Assets/Test.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        int[] arraa = { 564, 349, 984, 516, 208 };
+        for (int b = 0; b < arraa.Length; b++)
+        {
+            Debug.Log(arraa[b]);
+        }
+        for (int a = arraa.Length - 1; a >= 0; a--)
+        {
+            Debug.Log(arraa[a]);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
